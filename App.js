e16ed5bdb37b43bfef22bae8da5eb2dae752b1af
Caner Güldüren <canerguldurenn@gmail.com>
2025-07-06 11:19:42 +0300

    Add initial app structure with navigation

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { openDb, createTable } from "./database";
+import HomeScreen from "./screens/HomeScreen";
+import AddNoteScreen from "./screens/AddNoteScreen";
+import NoteDetailScreen from "./screens/NoteDetailScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [db, setDb] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const database = await openDb();
+      await createTable(database);
+      setDb(database);
+    })();
+  }, []);
+
+  if (!db) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home">
+          {(props) => <HomeScreen {...props} db={db} />}
+        </Stack.Screen>
+        <Stack.Screen name="AddNote">
+          {(props) => <AddNoteScreen {...props} db={db} />}
+        </Stack.Screen>
+        <Stack.Screen name="NoteDetail">
+          {(props) => <NoteDetailScreen {...props} db={db} />}
+        </Stack.Screen>
+        <Stack.Screen name="Favorites">
+          {(props) => <FavoritesScreen {...props} db={db} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
